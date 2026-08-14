a4636aea70b2f4b2bc07d2ad1f666bbcb6027626
growtrackid-wq <growtrack.id@gmail.com>
2026-08-14 23:21:35 +0800

    Update materi.py

--- pages/materi.py
+++ pages/materi.py
@@ -127,20 +127,20 @@ with tab_slide:
     with col1:
         with st.container(border=True):
             st.markdown("### 🩺 SLIDE MATERI")
-            st.caption("Diabetes Melitus, Tiroid, Adrenal, dan Gangguan Metabolik.")
+            st.caption("Semua Slide Materi yang telah dibahas.")
             url_endokrin = "https://drive.google.com/drive/folders/1PlPLzWMb4LHTJtM36ZvUn_I8dr2Aty8Z?usp=sharing"
             if st.session_state["is_logged_in"]:
-                st.link_button("📂 Buka Folder", url_endokrin, use_container_width=True, type="primary")
+                st.link_button("📂 Buka Folder", url_slide, use_container_width=True, type="primary")
             else:
-                st.button("🔒 Buka Folder Endokrin (Perlu Login)", disabled=True, use_container_width=True)
+                st.button("🔒 Buka Folder (Perlu Login)", disabled=True, use_container_width=True)
 
     with col2:
         with st.container(border=True):
             st.markdown("### CORETAN")
-            st.caption("Sistem Pencernaan, Hati, Saluran Empedu, dan Gastrointestinal.")
+            st.caption("Coretan tangan para tutor.")
             url_gastro = "https://drive.google.com/drive/folders/1wxjrjykHwZ-ZhF6SHj5MiRpQ5MAjFJKi?usp=sharing"
             if st.session_state["is_logged_in"]:
-                st.link_button("📂 Buka Folder Gastro", url_gastro, use_container_width=True, type="primary")
+                st.link_button("📂 Buka Folder", url_coretan, use_container_width=True, type="primary")
             else:
                 st.button("🔒 Buka Folder (Perlu Login)", disabled=True, use_container_width=True)
 
